b12e3d4248d91a9625aa5840974a9a005d229677
Akash <akashbytes@gmail.com>
2024-03-25 14:09:28 +0530

    modified..

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import {  RouterProvider, createBrowserRouter} from 'react-router-dom';
+import SignIn from './Components/sign_in';
+// import Navigation from './Components/Browse_Components/header';
+import BrowseComponent from './BrowseComponents';
+
+// import Navigation from './Components/Browse_Components/header';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ 
+
+const AppRouter = createBrowserRouter([
+  {path:"/",
+   element:<SignIn></SignIn>},
+   {
+    path:"/browse",
+    element:<BrowseComponent/>
+   }
+])
+
+
+
+
+  return (<div>
+<RouterProvider router={AppRouter}/>
+  </div>  );
 }
 
 export default App;
+
--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import NetFlixStore from './RStore/netflixStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+ <Provider store={NetFlixStore} >
     <App />
+    </Provider>
   </React.StrictMode>
 );
 
