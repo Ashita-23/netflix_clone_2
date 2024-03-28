91959e90a0cdf0a3a0c11e48df70b8b532522d4f
Akash <akashbytes@gmail.com>
2024-03-28 11:50:36 +0530

    error image.. search error image.. similer movie Tv suggetion card ...

---
+++ src/Components/SuggetionCards.js
@@ -0,0 +1,21 @@
+
+import { POSTER_API } from "../utils/apiOptions"
+import dummyImg from "../assets/noData.png"
+
+ const SuggestionCards=({data})=>{
+    return <div className="drop-shadow-lg m-2 flex flex-col w-4/12 h-[15.5rem] items-center overflow-hidden bg-black"  >
+  {!POSTER_API+data.backdrop_path?  <img src={POSTER_API+data.backdrop_path} alt="poster" className="w-[280px] m-0" key={ data.vote_count} ></img>
+  :<img src={dummyImg} alt="poster" className="w-[140px] m-[auto]" key={ data.vote_count} ></img>
+   }
+    <div className=" flex flex-col w-[100%] ">
+    <p className="text-[0.9rem] p-1 text-center">{data.title||data.name}</p>
+    <div className="text-green-500 text-sm py-2 px-1 flex justify-around ">
+    <span><i className="fa-regular fa-star"></i> {Math.round(data.vote_average)}</span> {" "}
+    <span><i className="fa-regular fa-eye"></i>  {data.vote_count/10}K</span> 
+    <button className="bg-red-600 text-sm text-white px-2 py-1 hover:bg-gray-500">Watch Now</button>
+    </div>
+    </div>
+    </div>
+}
+
+export default SuggestionCards
---
+++ src/Components/searchError.js
@@ -0,0 +1,11 @@
+import searchErrorimg from "../assets/sreactError.png"
+
+ const SearchErrorCards = ()=>{
+    return(<div className=" m-2   flex flex-col items-center p-1">
+    <img src={searchErrorimg} className="w-[200px] h-[200px]" />
+    <p className="text-white text-2xl font-medium m-2">Try something else...</p>
+    </div>)
+}
+
+
+export default SearchErrorCards
