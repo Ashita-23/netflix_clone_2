35e6340b501ca58051649c1c71bf5af0a2b5b4a8
Akash <akashbytes@gmail.com>
2024-03-28 11:53:41 +0530

    cards updated..

--- src/Components/videoCards.js
+++ src/Components/videoCards.js
@@ -12,22 +12,15 @@ const NowPlayingCards =({NowData,titleText})=>{
 
 
 
-    return(<>
-        <div className="p-2 relative"  >
+    return(<div className="p-2 relative"  >
        <h1 className="text-2xl font-semibold p-2 text-white" key={titleText.key} >{titleText.titleText} </h1>
-         <div className="flex overflow-x-scroll p">
+         <div className="flex overflow-scroll p-1 no-scrollbar ">
          {NowData?.map((data)=>{return(<img src={POSTER_API+data?.poster_path} alt="poster" className="w-[150px] mx-2" key={data?.vote_count} 
          onClick={()=> dispatch(addMovieID(data))}></img>)})}
-
          </div>
-     
-      
         </div>
+  
         
-      {/* <div className="border border-red-700 h-[60%] w-screen sticky bg-black backdrop-blur-sm -mt-[20%] flex justify-center items-center z-[100] ">
-      <Movies/>
-        </div>   */}
-        </>
     )
 }
 
