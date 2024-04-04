12022d158568b936fa38f516826f24da3436e9f5
Akash <akashbytes@gmail.com>
2024-04-04 17:56:52 +0530

    added netflixCardsData slice..

--- src/RStore/netflixStore.js
+++ src/RStore/netflixStore.js
@@ -3,11 +3,12 @@ import SearchToggle from "./searchToggle";
 import AccordionToggle from "./accordionToggle"
 import FormToogle from "./formToggle"
 import UserSlice from "./userSlice";
+import netflixCardsData from "./netflixCardsData";
 import movieId from "./movieId";
 import TvSearchToggle from "./tvSearchToggle";
 import SeriesId from "./seriesId"
 import My_List_id_Data from "./myList";
-import MY_LIST_DATA from "./mylistData"
+import MY_LIST_DATA from "./mylistData";
 
 
 const NetFlixStore = configureStore({
@@ -16,11 +17,12 @@ Search_Toggel : SearchToggle,
 Accordion_Toggle:AccordionToggle,
 Form_Toogle:FormToogle,
 User_Slice:UserSlice,
+Netflix_CardsData: netflixCardsData,
 Movie_Id:movieId,
 Tv_SearchToggle:TvSearchToggle,
 Series_Id:SeriesId,
-My_List_id_Data: My_List_id_Data,
-My_List_Data :MY_LIST_DATA,
+My_List_id_Data:My_List_id_Data,
+My_List_Data:MY_LIST_DATA,
     }
 })
 
